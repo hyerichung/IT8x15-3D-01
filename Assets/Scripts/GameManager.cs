f3d89d58b7faa14a961ec32f13f56432c6629c6a
hyerichung <hyerichungdev@gmail.com>
2024-10-17 03:50:57 +1300

    Add: draft of GameManager, should work on TextUI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public static float hp;
+  public static float life;
+  public static float cash;
+
+  public static float currentLevel;
+  public static float currentWave;
+
+  public static bool isAlive;
+
+  static float MAX_WAVES = 5;
+  static float MAX_LEVEL = 2;
+
+  void Awake()
+  {
+    hp = 300f;
+    life = 20f;
+    cash = 600f;
+    currentLevel = 1;
+    currentWave = 1;
+    isAlive = true;
+  }
+
+  void Start()
+  {
+    if (!isAlive) return;
+  }
+
+  public void GameOver()
+  {
+    isAlive = false;
+  }
+
+  public void Restart()
+  {
+    hp = 300f;
+    life = 20f;
+    cash = 600f;
+    currentLevel = 1;
+    currentWave = 1;
+    isAlive = true;
+  }
+
+  public void IncreaseHp(float health)
+  {
+    float addedHp = hp += health;
+
+    if (addedHp >= 100f)
+    {
+      hp = 100f;
+    }
+    else
+    {
+      hp = addedHp;
+    };
+  }
+
+  public void DecreaseHp(float health)
+  {
+    float decreasedHp = hp -= health;
+
+    if (decreasedHp <= 0)
+    {
+      hp = 0;
+    }
+    else
+    {
+      hp = decreasedHp;
+    }
+  }
+
+  public void DecreaseLife()
+  {
+    life--;
+  }
+
+  public void IncreaseCash(float money)
+  {
+    cash += money;
+  }
+
+  public void DecreaseCash(float money)
+  {
+    float decreasedCash = cash -= money;
+
+    if (decreasedCash <= 0)
+    {
+      cash = 0;
+    }
+    else
+    {
+
+      cash -= decreasedCash;
+    }
+  }
+
+  public void IncreaseLevel(float lv)
+  {
+    float increasedLevel = currentLevel += lv;
+
+    if (increasedLevel > MAX_LEVEL) return;
+
+    currentLevel = increasedLevel;
+  }
+
+  public void IncreaseWave(float wv)
+  {
+    float increasedWave = currentWave += wv;
+
+    if (increasedWave > MAX_WAVES) return;
+
+    currentWave = increasedWave;
+  }
+
+}
