5dccbab099c26ab246947edd9064c0a1399f64af
hyerichung <hyerichungdev@gmail.com>
2024-10-18 21:31:09 +1300

    Add: PlacedObjectDatabase, gridIndicatorPreview, CheckPlacementValidity

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -10,21 +10,31 @@ public class PlacementManager : MonoBehaviour
 
   [SerializeField]
   private GameObject gridIndicator; // grid indicator based on current mouse indicator
+  private Renderer gridIndicatorPreview;
 
-  [SerializeField]
-  private InputManager inputManager;
   [SerializeField]
   private Grid grid; // cell
   [SerializeField]
   private GameObject gridPlane; // bunch of cells
 
   [SerializeField]
-  private ObjectDatabaseScriptable database;
+  private InputManager inputManager;
+
+  [SerializeField]
+  private ObjectDatabaseScriptable database; // placeable object prefeb database
   private int selectedObjectIndex = -1;
+
+  // Dictionary of placed GameObject
+  private PlacedObjectDatabase placedObjectDatabase;
+  // List of placed GameObject
+  private List<GameObject> placedObjectList = new();
 
   private void Start()
   {
     StopPlacement(); // reset placement before start
+
+    placedObjectDatabase = new();
+    gridIndicatorPreview = gridIndicator.GetComponentInChildren<Renderer>();
   }
 
   private void Update()
@@ -34,9 +44,19 @@ public class PlacementManager : MonoBehaviour
     Vector3 mousePosition = inputManager.GetSelectedLayerPosition(); // get current mouse pointing position
     Vector3Int gridPosition = grid.WorldToCell(mousePosition); // convert world position to grid position 
 
+    bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+
+    gridIndicatorPreview.material.color = placementValidity ? Color.white : Color.red;
+
     mouseIndicator.transform.position = mousePosition;
     gridIndicator.transform.position = grid.CellToWorld(gridPosition); // convert grid position to world position
+  }
 
+  private bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+  {
+    Vector2Int selectedObjectSize = database.objectData[selectedObjectIndex].Size;
+
+    return placedObjectDatabase.IsObjectPlaceable(gridPosition, selectedObjectSize);
   }
 
   public void StartPlacement(int Id)
@@ -68,8 +88,17 @@ public class PlacementManager : MonoBehaviour
     Vector3 mousePosition = inputManager.GetSelectedLayerPosition();
     Vector3Int gridPosition = grid.WorldToCell(mousePosition);
 
+    bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+    if (placementValidity == false) return;
+
     GameObject newGameObject = Instantiate(database.objectData[selectedObjectIndex].Prefab);
     newGameObject.transform.position = grid.CellToWorld(gridPosition);
+
+    // store newGameObject to placedObjectList
+    placedObjectList.Add(newGameObject);
+
+    // store newGameObject to placedObjectDatabase
+    placedObjectDatabase.AddObject(gridPosition, database.objectData[selectedObjectIndex].Size, database.objectData[selectedObjectIndex].Id, placedObjectList.Count - 1);
 
   }
 
