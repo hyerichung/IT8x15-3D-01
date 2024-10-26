2f895833adb19768d78ee8c8be098aa0fda7ec6f
hyerichung <hyerichungdev@gmail.com>
2024-10-27 04:09:19 +1300

    Add: Sample2 scene, Hero prefeb, animation, controller, integrated with Wavepoints

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,47 +4,132 @@ using System.Collections;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
-	private Transform target;
-	private int wavepointIndex = 0;
+    private Transform target;
+    private int wavepointIndex = 0;
+    public float speed = 6.0f;
+    private Enemy enemy;
 
-	private Enemy enemy;
+    private Transform hero;
+    private float distance = 0.0f;
 
-	void Start()
-	{
-		enemy = GetComponent<Enemy>();
+    private Animator anim;
 
-		target = Waypoints.points[0];
-	}
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        target = Waypoints.points[0];
 
-	void Update()
-	{
-		Vector3 dir = target.position - transform.position;
-		transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        hero = GameObject.FindGameObjectWithTag("Hero").transform;
+        anim = GetComponent<Animator>();
+    }
 
-		if (Vector3.Distance(transform.position, target.position) <= 0.4f)
-		{
-			GetNextWaypoint();
-		}
+    private void Update()
+    {
+        enemy.speed = enemy.startSpeed;
+        anim = WaveSpawner.anim;
 
-		enemy.speed = enemy.startSpeed;
-	}
+        Vector3 dir = target.position - transform.position;
+        //transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        //transform.LookAt(target);
 
-	void GetNextWaypoint()
-	{
-		if (wavepointIndex >= Waypoints.points.Length - 1)
-		{
-			EndPath();
-			return;
-		}
+        distance = Vector3.Distance(transform.position, hero.transform.position);
 
-		wavepointIndex++;
-		target = Waypoints.points[wavepointIndex];
-	}
+        if (distance <= 5.5f)
+        {
+            transform.Translate(0.001f, 0.001f, 0.001f);
+            MonsterAttack();
+        }
+        else
+        {
+            MonsterNormalMovement(dir);
+        }
 
-	void EndPath()
-	{
-		PlayerStats.Lives--;
-		WaveSpawner.EnemiesAlive--;
-		Destroy(gameObject);
-	}
+        //if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        //{
+        //    GetNextWaypoint();
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Hero")
+        {
+            //Debug.Log("trigger");
+            enemy.startHealth -= 10.0f;
+            if (enemy.startHealth == 0)
+            {
+                EndPath();
+            }
+        }
+    }
+
+
+    void MonsterNormalMovement(Vector3 dir)
+    {
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        transform.LookAt(target);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+    }
+
+    void MonsterAttack()
+    {
+        if (anim != null)
+        {
+            switch (anim.name)
+            {
+                case "succubus_warrior(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 4);
+                    break;
+                case "Frogman_beast_blu(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 10);
+                    break;
+                case "iguana_warrior(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 3);
+                    break;
+                case "Monster_rabbit(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 3);
+                    break;
+                case "Goblins_all(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 4);
+                    break;
+                case "golem_lava(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 5);
+                    break;
+                case "ent_1(Clone)":
+                    anim.SetInteger("battle", 1);
+                    anim.SetInteger("moving", 5);
+                    break;
+            }
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath()
+    {
+        PlayerStats.Lives--;
+        WaveSpawner.EnemiesAlive--;
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,17 @@ public class PlayerController : MonoBehaviour
 {
   [SerializeField]
   private KeyCode jumpKeyCode = KeyCode.Space;
+  private KeyCode attackKeyCode = KeyCode.Z;
+  private KeyCode gettingHitKeyCode = KeyCode.X;
+  private KeyCode dieKeyCode = KeyCode.C;
 
   private Movement3D movement3D;
+  private Animator animator;
 
   void Awake()
   {
     movement3D = GetComponent<Movement3D>();
+    animator = GetComponent<Animator>();
   }
 
   void Update()
@@ -19,6 +24,33 @@ public class PlayerController : MonoBehaviour
     // x, z movement
     float x = Input.GetAxisRaw("Horizontal"); // left/right arrow
     float z = Input.GetAxisRaw("Vertical"); // up/down arrow
+
+    if (z == 0)
+    {
+      animator.SetBool("isRunning", false);
+    }
+    else
+    {
+      animator.SetBool("isRunning", true);
+    }
+
+    // Z key -> attack
+    if (Input.GetKeyDown(attackKeyCode))
+    {
+      animator.SetTrigger("isAttacking");
+    }
+
+    // X key -> attack
+    if (Input.GetKeyDown(gettingHitKeyCode))
+    {
+      animator.SetTrigger("isGettingHit");
+    }
+
+    // C key -> die
+    if (Input.GetKeyDown(dieKeyCode))
+    {
+      animator.SetTrigger("isDied");
+    }
 
     // vector with x, z direction
     Vector3 moveDirection = new Vector3(x, 0, z);
